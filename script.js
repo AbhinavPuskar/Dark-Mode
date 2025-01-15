49996dc34b4c22f4903897886e380baacdd189cf
AbhinavPuskar <abhinavpuskar>
2025-01-15 20:42:35 +0530

    changes made

--- script.js
+++ script.js
@@ -1,30 +1,17 @@
-let round = document.querySelector(".round");
-let btn = document.querySelectorAll("button");
-let shape = document.querySelector("#square");
 
-//to change the Color
-btn[0].addEventListener("click", changeColor);
-
-function generateColor(){
-    let color = Math.floor(Math.random() * 255);
-    return color;
-}
-
-function changeColor(){
-    let newColor = `rgb(${generateColor()}, ${generateColor()}, ${generateColor()})`;
-    round.style.backgroundColor = newColor;
-}
-
-//to change the Shape
-btn[1].addEventListener("click", changeShape);
-let arr=["square", "round", "diamond", "triangle", "arrow", "frame", "star","cross", "left-point", "right-point", "parallal", "cheg"];
-
-function randomShape(){
-    let idx = Math.floor(Math.random() * arr.length);
-    return arr[idx];
-}
-
-function changeShape(){
-    let idname = randomShape();
-    shape.id = idname;
-}
+const toggle = document.getElementById('toggle-container');
+let circle = document.getElementById('circle');
+const day = document.getElementById('day');
+const night = document.getElementById('night');
+const body = document.body
+console.log(body.style);
+toggle.addEventListener('click', (e) => {
+    // console.log("Hello world!" , e);
+    document.body.classList.toggle('dark-mode');
+    circle.style.transform = 'translateX(calc(100% + 10px))';
+    document.body.style.transition = 'background-color 1s, color 1s';
+    // circle.style.transform = 'translate(60px)';
+    circle.style.transition = 'transform .5s'
+    circle.style.overflow = 'hidden';
+    day.style.visibility = 'visible';
+})
